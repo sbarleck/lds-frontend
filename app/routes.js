f6142befb2915eea9f22a886625abaf6b020a8b6
YuriSS <yuri.s.dos.santos@gmail.com>
2015-11-15 04:56:06 -0200

    Melhoria na requisicao das instituicoes

--- app/routes.js
+++ app/routes.js
@@ -1,29 +1,56 @@
-var fs     = require('fs');
-var path   = require('path');
-var dir    = 'routes/';
+var fs          = require('fs');
+var path        = require('path');
+var dir         = 'routes/';
 
 var baseHandler = {};
 
-function handlingError() {
-  return function(err, files) {
-    if(err) {
-      return console.error('Error: %s', err);
+baseHandler.handlingResponse = function(callback, atributes) {
+    return function(req, res) {
+        callback(res, atributes);
     }
-    files.forEach(requiringFile);
-  }
+};
+
+baseHandler.handlingRequest = function(res, atributes) {
+    baseHandler.request(baseHandler.config.webservice + atributes.uri, baseHandler.handlingError(res, atributes));
+};
+
+baseHandler.handlingError = function(res, atributes) {
+    return function(err, response, body) {
+        var result = {}
+
+        result.pageTitle = baseHandler.config.pageTitle;
+        result[atributes.key] = JSON.parse(body);
+
+        res.render(atributes.view, result);
+    }
+};
+
+/*
+    * Percorre todos arquivos da pasta routes e da um require
+*/
+
+function getFiles(application, express, request, config) {
+
+    baseHandler.app     = application;
+    baseHandler.express = express;
+    baseHandler.request = request;
+    baseHandler.config  = config;
+
+    fs.readdir(path.join(__dirname, dir), handlingError());
+
+};
+
+function handlingError() {
+    return function(err, files) {
+        if(err) {
+            return console.error('Error: %s', err);
+        }
+        files.forEach(requiringFile);
+    }
 }
 
 function requiringFile(file) {
-  require('./' + dir + file)(baseHandler);
+    require('./' + dir + file)(baseHandler);
 }
 
-module.exports = function(application, express, request, config) {
-
-  baseHandler.app     = application;
-  baseHandler.express = express;
-  baseHandler.request = request;
-  baseHandler.config  = config;
-
-  fs.readdir(path.join(__dirname, dir), handlingError());
-
-};
+module.exports = getFiles;
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,37 +4,7 @@ module.exports = function(baseHandler) {
 
     var router = baseHandler.express.Router();
 
-    function handlingResponse(callback, atributes) {
-        return function(req, res) {
-            callback(res, atributes);
-        }
-    }
-
-    function handlingRequest(res, atributes) {
-        baseHandler.request(baseHandler.config.webservice + atributes.uri, handlingError(res, atributes));
-    }
-
-    function handlingError(res, atributes) {
-        return function(err, response, body) {
-            var result = {}
-
-            result.pageTitle = baseHandler.config.pageTitle;
-            result[atributes.key] = JSON.parse(body);
-
-            console.log(result);
-
-            res.render(atributes.view, result);
-        }
-    }
-
-    router.get('/', handlingResponse(handlingRequest, {uri: '/instituicao', key: 'instituicoes', view: 'index'}));
-
-    // router.get('/', function(require, response) {
-    //     request('http://localhost:8080/linha-instituicao', function(err, res, body) {
-    //         var result = JSON.parse(body)['_embedded']['linha_instituicao'];
-    //         response.render('index', {pageTitle: 'Linha de Saúde', instituicoes: result});
-    //     });
-    // });
+    router.get('/', baseHandler.handlingResponse(baseHandler.handlingRequest, {uri: '/instituicao', key: 'instituicoes', view: 'index'}));
 
     baseHandler.app.use('/', router);
 

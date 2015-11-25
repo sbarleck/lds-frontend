bf9e5417f7e6399165125527190355a132a92c44
YuriSS <yuri.s.dos.santos@gmail.com>
2015-11-25 21:02:10 -0200

    Mapa implementado

--- public/scripts/mapa.js
+++ public/scripts/mapa.js
@@ -2,8 +2,8 @@ var lds["map"] = (function(){
     var map;
     function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 8
         });
     }
     

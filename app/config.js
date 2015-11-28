cab1d04d3d8454209cc4334094f5ddbab65e68ec
YuriSS <yuri.s.dos.santos@gmail.com>
2015-11-28 16:07:56 -0200

    Remove bower

--- app/config.js
+++ app/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     port: process.env.PORT || 3000,
     webservice: 'http://localhost:3333/api',
-    pageTitle: 'Linha de Saúde'
+    pageTitle: 'Linha de Saúde',
+    defaultGeo: [-30.058860, -51.167885]
 };
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,7 @@ module.exports = function(baseHandler) {
 
     var router = baseHandler.express.Router();
 
-    router.get('/', baseHandler.handlingResponse(baseHandler.handlingRequest, {uri: '/instituicao', key: 'instituicoes', view: 'index'}));
+    router.get('/', baseHandler.handlingResponse(baseHandler.handlingRequest, {uri: '/instituicao/near', body: baseHandler.config.defaultGeo, key: 'instituicoes', view: 'index'}));
 
     baseHandler.app.use('/', router);
 
